66371c32e55a1cc36e2b10a8f622ef3aa983539c
Fozi Chris <kamufozi679@gmail.com>
2025-06-18 14:45:04 +0200

    just pushed something

--- tutu.js
+++ tutu.js
@@ -13,11 +13,11 @@ var fullName = "".concat(firstName, " ").concat(surName);
 function pluto(a, b) {
     return a + (b || 0);
 }
-console.log(pluto(0,'hey'));
+console.log(pluto(0));
 function divide(a, b) {
     return a / b;
 }
-// var dateOfBirth = new Date(1990, 4, 7);
+// let dateOfBirth = new Date(1990, 4, 7);
 var firsnetName = "Bob";
 var age = 31;
 var created = new Date(2019, 11, 6);
@@ -30,7 +30,60 @@ var surname = last;
 var first = "Bob";
 // const last = "Smith";
 var fulnlName = "".concat(first, " ").concat(surname);
-let what=Date.now;
-console.log(what);
-const dateOfBirth = new Date(1990, 4, 7);
-console.log(dateoOfBirth.g)
+var dateOfBirth = new Date(1990, 4, 7);
+console.log(dateOfBirth.getDate);
+// console.log(dateOfBirth.addDays(2));
+// function calculateRenewal(startDate:Date):Date {
+//   const result = new Date(startDate);
+//   result.setDate(result.getDate() + 30);
+//   return result;
+// }
+// let whatCanIHold: void;
+// whatCanIHold = null;
+// whatCanIHold = "something";
+// const numbers: Array<number> = [1,'two',true];
+var items = [];
+var array = [1, 2, 3];
+console.log(array);
+function logScores(firstName) {
+    var scores = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        scores[_i - 1] = arguments[_i];
+    }
+    console.log(firstName, scores);
+}
+logScores("Ben", 50, 75, 85); // outputs Ben and [50, 75, 85]
+// logScores("Mike", 90, 65, "65");
+var tomScore = ["Tom", 70];
+var tomScores = ["Tom", 70, 89];
+var benScores = ['Ben', 70, 50, 80];
+function doSomeAction(status) {
+    switch (status) {
+        case "Pending":
+            // some code
+            break;
+        case "Working":
+            // some code
+            break;
+        case "Complete":
+            // some code
+            break;
+        case "canceled":
+            //some code
+            break;
+        default:
+            neverReached(status);
+            break;
+    }
+}
+function neverReached(never) {
+}
+doSomeAction("Pending");
+function add(a, b) {
+    if (typeof a === "number" && typeof b === "number") {
+        return a + b;
+    }
+    return 0;
+}
+console.log(add(4, 5));
+
--- tutu.ts
+++ tutu.ts
@@ -43,12 +43,82 @@ const first = "Bob";
 const fulnlName = `${first} ${surname}`;
 const dateOfBirth = new Date(1990, 4, 7);
 console.log(dateOfBirth.getDate)
-console.log(dateOfBirth.addDays(2));
-function calculateRenewal(startDate:Date):Date {
-  const result = new Date(startDate);
-  result.setDate(result.getDate() + 30);
-  return result;
+// console.log(dateOfBirth.addDays(2));
+// function calculateRenewal(startDate:Date):Date {
+//   const result = new Date(startDate);
+//   result.setDate(result.getDate() + 30);
+//   return result;
+// }
+// let whatCanIHold: void;
+// whatCanIHold = null;
+// whatCanIHold = "something";
+// const numbers: Array<number> = [1,'two',true];
+const items:number[]=[]
+const array = [1, 2, 3];
+console.log(array);
+function logScores(firstName:string, ...scores:number[]) {
+  console.log(firstName, scores);
 }
-let whatCanIHold: void;
-whatCanIHold = null;
-whatCanIHold = "something";
+
+logScores("Ben", 50, 75, 85); // outputs Ben and [50, 75, 85]
+// logScores("Mike", 90, 65, "65");
+
+const tomScore: [name:string, ...score:number[]] = ["Tom", 70];
+const tomScores: (string|number)[] = ["Tom", 70,89];
+const benScores:[name:string,...score:number[]]=['Ben',70,50,80]
+type Status = "Pending" | "Working" | "Complete"|"canceled";
+
+function doSomeAction(status: Status) {
+  switch (status) {
+    case "Pending":
+      // some code
+      break;
+    case "Working":
+      // some code
+      break;
+    case "Complete":
+      // some code
+      break;
+    case "canceled":
+      //some code
+      break;
+    default:
+      neverReached(status)
+      break;
+  }
+}
+function neverReached(never:never){
+}
+doSomeAction("Pending");
+function add(a: unknown, b: unknown) {
+   if (typeof a === "number" && typeof b === "number") {
+    return a + b;
+  }
+  return 0
+}
+console.log(add("4", 5));
+
+async function getData(path: string): Promise<unknown> {
+  const response = await fetch(path);
+  return await response.json();
+}
+
+type Person = {
+  id: string;
+  name: string;
+};
+function isPerson(person: any): person is Person {
+  return "id" in person && "name" in person;
+}
+async function getPerson(id: string): Promise<Person | null> {
+  const person = await getData("/people/1");
+  if (person) {
+    return person;
+  }
+  return null;
+}
+let tue:[string,number];
+tue=[29,'jellu'
+]
+
+let 
